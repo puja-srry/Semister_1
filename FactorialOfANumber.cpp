c498107313ccb3ddabf6ca9bab9ca111b92aa70b
Gali Prasana Puja Srry <pujasrrygali@gmail.com>
2025-12-02 10:50:10 +0530

    Add files via upload

---
+++ FactorialOfANumber.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int i,n;
+	unsigned long long fact=1;
+	printf("enter a number:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		fact*=i;
+	}
+	
+	printf("factorial of %llu = %d\n",fact,n);
+	
+	return 0;
+}
